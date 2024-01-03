902dabc8c56fb8b754f9e4a1c4966a63098a5fc7
lee14k <kmkovach@mtu.edu>
2024-01-02 20:22:44 -0500

    needed to create a dummy navigator?

--- src/main.server.ts
+++ src/main.server.ts
@@ -3,6 +3,9 @@ global['requestAnimationFrame'] = (callback) => {
     return setTimeout(callback, 0);
 };
 
+// Mocking navigator with type assertion
+global['navigator'] = { userAgent: 'node.js' } as unknown as Navigator;
+
 // Now, the rest of your imports and code
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';

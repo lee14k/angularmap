cb659f4ffbabd4f9c195802eb20826603f6701ff
lee14k <kmkovach@mtu.edu>
2024-01-02 19:48:30 -0500

    structuring and preparing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { MapComponent } from '../components/map/map.component'; // Import the missing module from the correct file path
 
-export const routes: Routes = [];
+export const routes: Routes = [  { path: 'map', component: MapComponent },
+];
---
+++ src/components/map/map.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import * as am5 from "@amcharts/amcharts5";
+import * as am5map from "@amcharts/amcharts5/map";
+import am5geodata_worldLow from "@amcharts/amcharts5-geodata/worldLow";
+
+@Component({
+    selector: 'app-map',
+    templateUrl: './map.component.html',
+    styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit, OnDestroy {
+    private root: am5.Root | undefined;
+
+    constructor() { }
+
+    ngOnInit(): void {
+        this.root = am5.Root.new("chartdiv");
+
+        let chart = this.root.container.children.push(am5map.MapChart.new(this.root, {
+            panX: "rotateX",
+            panY: "rotateY",
+            projection: am5map.geoMercator()
+        }));
+
+        chart.series.push(am5map.MapPolygonSeries.new(this.root, {
+            geoJSON: am5geodata_worldLow
+        }));
+    }
+
+    ngOnDestroy(): void {
+        if (this.root) {
+            this.root.dispose();
+        }
+    }
+}
